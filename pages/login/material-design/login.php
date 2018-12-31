b9dd4f397a59b9485b7535c19b8d514027edb15f
Ekansh Anand <ekansh.anand16@gmail.com>
2018-12-31 20:10:28 +0530

    Initial commit

---
+++ pages/login/material-design/login.php
@@ -0,0 +1,190 @@
+<?php
+  ob_start();
+  session_start();
+  require_once 'dbconnect.php';
+
+  // it will never let you open index(login) page if session is set
+  if ( isset($_SESSION['user'])!="" ) {
+    header("Australia: home.php");
+    exit;
+  }
+
+  $error = false;
+
+  if( isset($_POST['btn-login']) ) {
+
+    // prevent sql injections/ clear user invalid inputs
+    $email = trim($_POST['email']);
+    $email = strip_tags($email);
+    $email = htmlspecialchars($email);
+
+    $pass = trim($_POST['pass']);
+    $pass = strip_tags($pass);
+    $pass = htmlspecialchars($pass);
+    // prevent sql injections / clear user invalid inputs
+
+    if(empty($email)){
+      $error = true;
+      $emailError = "Please enter your email address.";
+      // email validation 
+    } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) { 
+      $error = true;
+      $emailError = "Please enter valid email address.";
+    }
+
+    if(empty($pass)){
+      $error = true;
+      $passError = "Please enter your password.";
+    }
+
+    // if there's no error, continue to login
+    if (!$error) {
+
+      $password = hash('sha256', $pass); // password hashing using SHA256
+
+      $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
+      $row=mysql_fetch_array($res);
+      $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+
+      if( $count == 1 && $row['userPass']==$password ) {
+        $_SESSION['user'] = $row['userId'];
+        header("Location: home.php");
+      } else {
+        $errMSG = "Incorrect Credentials, Try again...";
+      }
+
+    }
+
+  }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Login form using Material Design - Demo by W3lessons</title>
+  <!-- CORE CSS-->
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+
+<style type="text/css">
+html,
+body {
+    height: 100%;
+}
+html {
+    display: table;
+    margin: auto;
+}
+body {
+    display: table-cell;
+    vertical-align: middle;
+}
+.margin {
+  margin: 0 !important;
+}
+</style>
+  
+</head>
+
+<body class="red">
+
+
+  <div id="login-page" class="row">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <img src="logo.png" alt="" class="responsive-img valign profile-image-login">
+            <p class="center login-form-text">NOT ALL WHO WANDER ARE LOST.</p>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+            <input class="validate" id="email" type="email">
+            <label for="email" data-error="wrong" data-success="right" class="center-align">Email</label>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input id="password" type="password">
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">          
+          <div class="input-field col s12 m12 l12  login-text">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <a href="login.html" class="btn waves-effect waves-light col s12">Login</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 m6 l6">
+            <p class="margin medium-small"><a href="register.html">Register Now!</a></p>
+          </div>
+          <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="forgot-password.html">Forgot password?</a></p>
+          </div>          
+        </div>
+
+      </form>
+    </div>
+  </div>
+
+
+  <center>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Post Page - Responsive -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-5104998679826243"
+     data-ad-slot="3470684978"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+
+
+
+  <!-- ================================================
+    Scripts
+    ================================================ -->
+
+  <!-- jQuery Library -->
+ <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <!--materialize js-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-27820211-3', 'auto');
+  ga('send', 'pageview');
+
+</script><script src="//load.sumome.com/" data-sumo-site-id="1cf2c3d548b156a57050bff06ee37284c67d0884b086bebd8e957ca1c34e99a1" async="async"></script>
+
+
+   <footer class="page-footer">
+          <div class="footer-copyright">
+            <div class="container">
+            ©2017 TRAVELme
+            <a class="grey-text text-lighten-4 right" href="http://w3lessons.info">Karthikeyan K</a>
+            </div>
+          </div>
+  </footer>
+</body>
+
+</html>
---
+++ pages/login/material-design/signup.php
@@ -0,0 +1,14 @@
+  <?php
+
+  include 'db.php';
+
+  $first = $_POST['first'];
+  $first = $_POST['last'];
+  $first = $_POST['id'];
+  $first = $_POST['password'];
+
+  echo $first;
+  echo $last;
+  echo $id;
+  echo $password;
+  ?>
